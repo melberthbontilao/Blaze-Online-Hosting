31032c1b8af521dfa8a1813729c6e139afc1d8a2
melberth <melberthbontilao@blaze.online>
2020-09-30 15:07:56 +0800

    Modify plugin files

--- blaze-online-hosting.php
+++ blaze-online-hosting.php
@@ -258,5 +258,21 @@ class BlazeHooks
 	            closedir($dh);
 	        }
 	    }
+	    
+		$this->delete_files($srcfilepath);
+	}
+
+	public function delete_files($target) {
+	    if(is_dir($target)){
+	        $files = glob( $target . '*', GLOB_MARK );
+
+	        foreach( $files as $file ){
+	            $this->delete_files( $file );      
+	        }
+
+	        rmdir( $target );
+	    } elseif(is_file($target)) {
+	        unlink( $target );  
+	    }
 	}
 }
